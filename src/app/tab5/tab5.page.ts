ba631f62b6ca3cd6eb2040a6a4fd787c7772af36
emnalakani <emnalakani98@gmail.com>
2020-04-25 14:45:01 +0100

    25/04

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab5',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
+  sliderConfig = {
+  spaceBetween: 2,
+  
+  slidesPerView: 1.8
 
+}
+  selectedSlide:any;
+segment = 0 ;
+sliderOptions= {
+intialSlide: 0,
+slidesPerview: 1,
+speed: 400  
+}
   constructor() { }
 
   ngOnInit() {
   }
+async segmentChanged(ev){
+  await this. selectedSlide.slideTo(this.segment);
+}
+async  slideShanged(slides : IonSlides){
+this.selectedSlide= slides;
+slides.getActiveIndex().then(selectedIndex =>{
+  this.segment = selectedIndex; 
+})
 
+}
 }
